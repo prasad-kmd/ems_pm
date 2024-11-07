911902f2b6430623c975551e59dac7be73a0adfc
PRASAD MADHURANGA <102537177+prasad-kmd@users.noreply.github.com>
2024-11-08 00:05:03 +0530

    Semantic-UI theming Added

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -8,14 +8,37 @@ if (!isset($_SESSION['admin_id'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Administrator Dashboard</title>
-    <link rel="stylesheet" href="assets/css/dashboard.css">
-    <link rel="stylesheet" href="assets/font/fonts.css"/>
+    <link rel="stylesheet" href="assets/css/semantic.css">
+    <link rel="stylesheet" href="assets/font/fonts.css" />
 </head>
+
 <body>
+    <div class="ui inverted segment">
+        <div class="ui inverted secondary menu">
+            <div class="item" width="50px">
+                <img src="assets/images/logo.webp" alt="Company Logo" width="50px">
+            </div>
+            <a class="active item">
+                Admin Dashboard
+            </a>
+            <a class="item">
+                Jobs
+            </a>
+            <a class="item">
+                Locations
+            </a>
+            <div class="right menu">
+                <div class="item">
+                    <div class="ui primary button">Log out</div>
+                </div>
+            </div>
+        </div>
+    </div>
     <h1>Welcome, <?php echo $_SESSION['admin_name']; ?>!</h1>
     <h2>Administrator Dashboard</h2>
 
@@ -27,5 +50,9 @@ if (!isset($_SESSION['admin_id'])) {
         <a href="event_calendar.php">Event Calendar</a>
         <a href="reports.php">Generate Reports</a>
     </div>
+    <script src="assets/js/semantic.js"></script>
+    <script src="assets/js/jquery-3.7.1.min.js"></script>
+    <a href="logout.php">Logout</a>
 </body>
+
 </html>
